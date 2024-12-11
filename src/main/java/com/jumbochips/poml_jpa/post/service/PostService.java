1a9f093fea8511ca91290d5693d51f98ba039d3e
신유빈 <jumbochips@JumboChips.local>
2024-12-11 22:54:19 +0900

    12/11 update

--- src/main/java/com/jumbochips/poml_jpa/post/service/PostService.java
+++ src/main/java/com/jumbochips/poml_jpa/post/service/PostService.java
@@ -4,13 +4,14 @@ import com.jumbochips.poml_jpa.post.domain.Post;
 import com.jumbochips.poml_jpa.post.dto.PostRequestDto;
 import com.jumbochips.poml_jpa.post.dto.PostResponseDto;
 import com.jumbochips.poml_jpa.post.repository.PostRepository;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface PostService {
     List<PostResponseDto> getAllPost(Long userId);
     PostResponseDto getPost(Long postId);
-    PostResponseDto CreatePost(PostRequestDto postRequestDto);
+    PostResponseDto createPost(PostRequestDto postRequestDto);
     PostResponseDto updatePost(Long postId, PostRequestDto postRequestDto);
-    PostResponseDto deletePost(Long postId);
+    ResponseEntity<?> deletePost(Long postId);
 }
