1af835d0398122fe0c90b98d964515375bf6b845
jumbochips <jumbochips1@gmail.com>
2024-12-19 23:57:04 +0900

    GuestbookRepository 미사용 오류 로직 삭제

--- src/main/java/com/jumbochips/poml_jpa/guestBook/repository/GuestbookRepository.java
+++ src/main/java/com/jumbochips/poml_jpa/guestBook/repository/GuestbookRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface GuestbookRepository extends JpaRepository<GuestBook, Long> {
-    public List<GuestBook> findAllByUserId(Long userId);
 }
